b5c79345e015bf37e160f3dc03fe42d621ecf998
Aaron Yang <aaronyangy98@gmail.com>
2026-04-08 16:38:46 -0700

    IE1 push

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+import MyApp from "./MyApp";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//create container
+const container = document.getElementById("root");
+
+//create root
+const root = ReactDOMClient.createRoot(container);
+
+//Initial render: Render an element to the Root
+root.render(<MyApp/>);
